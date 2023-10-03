4f451778ca30c4a705acc3e6627acfbb274a9db0
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-10-03 06:56:43 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -22,44 +22,53 @@ export const inputFilteredContacts = createAction('app/inputFilteredContacts')
 
 
 
-export const rootReducer1 = createReducer(initialState, builder => builder
-  .addCase(getLocalStorage, (state, action)=>{})
-  .addCase (addContact, (state, action)=>{})
-  .addCase(deleteContact, (state, action)=>{})
-  .addCase (inputFilteredContacts,(state, action)=>{})
-)
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'app/getLocalStorage':
-      return {
-        ...state,
-        contacts: action.payload,
-      };
-
-    case 'app/addContact':
-      const newContact = action.payload;
+export const rootReducer = createReducer(initialState, builder => builder
+  .addCase(getLocalStorage, (state, action)=>{
+    state.contacts+=action.payload})
+  .addCase (addContact, (state, action)=>{
+    const newContact = action.payload;
       const updContacts = [...state.contacts, newContact];
-      return {
-        ...state,
-        contacts: updContacts,
-      };
+      state.contacts+=updContacts
+  })
+  .addCase(deleteContact, (state, action)=>{
+    state.contacts+=action.payload
+  })
+  .addCase (inputFilteredContacts,(state, action)=>{
+    state.filter+=action.payload
+  })
+)
+// const rootReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'app/getLocalStorage':
+//       return {
+//         ...state,
+//         contacts: action.payload,
+//       };
 
-    case 'app/deleteContact':
-      return {
-        ...state,
-        contacts: action.payload,
-      };
+//     case 'app/addContact':
+//       const newContact = action.payload;
+//       const updContacts = [...state.contacts, newContact];
+//       return {
+//         ...state,
+//         contacts: updContacts,
+//       };
 
-    case 'app/inputFilteredContacts':
-      return {
-        ...state,
-        filter: action.payload,
-      };
+//     case 'app/deleteContact':
+//       return {
+//         ...state,
+//         contacts: action.payload,
+//       };
 
-    default:
-      return state;
-  }
-};
+//     case 'app/inputFilteredContacts':
+//       return {
+//         ...state,
+//         filter: action.payload,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
 
 
 export const store = configureStore ({
